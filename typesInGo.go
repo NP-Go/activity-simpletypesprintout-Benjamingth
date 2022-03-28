8f8ef3c1adcafa7b7918a46283b699e78e64e6ff
Benjamingth <benjamin.gth@hotmail.com>
2022-03-28 16:57:29 +0800

    Added types

--- typesInGo.go
+++ typesInGo.go
@@ -1,9 +1,17 @@
 package main
 
+import "fmt"
+
 //Insert variables declarations here based on activity
 
 func tellMeTypes() {
 	//insert code here to print out types of variables
+	age := 20
+	weight := 73.0
+	firstname := "Luke"
+	lastname := "Skywalker"
+
+	fmt.Printf("%T %T %T %T\n", age, weight, firstname, lastname)
 }
 
 func main() {
